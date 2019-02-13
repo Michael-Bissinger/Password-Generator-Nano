8cff14a319e392682fd2254a46b421371e44a404
Michael Bissinger <Michael.Bissinger@inb.de>
2019-02-13 15:16:22 +0100

    Programming Controller. Now I just need to add the button

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,32 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Label label;
+    @FXML
+    private Text actiontarget;
+
+
+    @FXML
+    private void handleSubmitButtonAction(ActionEvent event) {
+        //System.out.println("You clicked me!");
+        //label.setText("Hello World!");
+        actiontarget.setText("Button to start was pressed!"); // Build main application here
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // ToDo
+    }
 
 }
