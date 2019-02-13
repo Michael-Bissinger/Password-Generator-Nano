9db2e21bfb0edb93cf7d5444309fb02bc9df1521
Michael Bissinger <Michael.Bissinger@inb.de>
2019-02-13 14:43:42 +0100

    Changing window size and adding title

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,9 +11,15 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+
+        primaryStage.setTitle("Password Generator Nano");
+        primaryStage.setScene(new Scene(root, 300, 500));
         primaryStage.show();
+
+        
+
+
     }
 
 
